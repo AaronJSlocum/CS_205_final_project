8bb8ff9654abf44ab38253e90b9b4bb07605a56c
AaronJSlocum <54634528+AaronJSlocum@users.noreply.github.com>
2020-04-08 20:43:41 -0400

    Created initial board and tile classes

---
+++ Board.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Board {
+    //Fields
+    private Tile[] tile_array;
+    private int num_houses;
+    private int num_hotels;
+    private int bank_money;
+    private int[] player_position;
+    
+
+}
---
+++ Tile.java
@@ -0,0 +1,13 @@
+public class Tile {
+    //Fields
+    private int tile_position;
+    private string tile_name;
+
+    //Constructor with position and name
+    public Tile(int tile_position, string tile_name){
+        this.tile_position = tile_position;
+        this.tile_name = tile_name;
+    }
+
+
+}
