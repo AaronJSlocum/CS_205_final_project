ab376aa5daedd34a65244fceb0398bf0bb4aec0a
eepearso <44347294+eepearso@users.noreply.github.com>
2020-04-11 18:05:52 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class Player {
+
+   //fields
+   private String name;
+   private int[] tile_position;
+   private int cash_val;
+   private Tile[] properties;
+   private boolean jail;
+   private boolean bankruptcy;
+   
+   public Player(){
+   
+   }
+   
+}
