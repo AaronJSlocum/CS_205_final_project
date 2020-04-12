90d6d42ba2d1ba01efc5f276f67bc4dfa1b54475
eepearso <44347294+eepearso@users.noreply.github.com>
2020-04-12 10:19:12 -0400

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,14 @@
+public class Card {
+  //fields 
+  private String card_name;
+  private String card_description;
+  private int cardnum;
+  
+  
+  //public Card(card_name, card_description, cardnum) {
+    this.card_name = card_name;
+    this.card_description = card_description;
+    this.cardnum = cardnum;
+    
+    }
+}
