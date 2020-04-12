60910a1782497f4819063c589b60b3b18b25f130
eepearso <44347294+eepearso@users.noreply.github.com>
2020-04-12 10:59:16 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -5,7 +5,7 @@ public class Card {
   private int cardnum;
   
   
-  public Card(string card_name, string card_description, int cardnum) {
+  public Card(String card_name, String card_description, int cardnum) {
     this.card_name = card_name;
     this.card_description = card_description;
     this.cardnum = cardnum;
