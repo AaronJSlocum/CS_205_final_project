e3d6f80477d41b42cae1cb273b2d79d824508ca2
eepearso <44347294+eepearso@users.noreply.github.com>
2020-04-26 12:47:58 -0400

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -2,11 +2,13 @@ public class Tile {
     //Fields
     private int tile_position;
     private String tile_name;
+    private int num_house;
 
     //Constructor with position and name
     public Tile(int tile_position, String tile_name){
         this.tile_position = tile_position;
         this.tile_name = tile_name;
+        this.num_house = num_house;
     }
 
 
